e1d875591d42a1e9c4688ec4d1aa8d4dde29887f
Ramon Chavez <rachavez1974@gmail.com>
2019-09-04 15:23:13 -0400

    Add gem rails_12factor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
-//= require bootstrap-sprockets
+//= require bootstrap-sprockets 
 //= require activestorage
 //= require turbolinks
 //= require_tree .
