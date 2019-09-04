9b87a01432805465925b13b2964afb0ad4f18a90
Ramon Chavez <rachavez1974@gmail.com>
2019-09-03 23:28:41 -0400

    Finish menus static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get '/about', to: 'static_pages#about', as: 'about'
+  get '/menus', to: 'static_pages#menus', as: 'menus'
+  get '/contact', to: 'static_pages#contact', as: 'contact'
 end
