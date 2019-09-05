1ede6cc9dabf2432826534e6e463a6042e495d16
Ramon Chavez <rachavez1974@gmail.com>
2019-09-04 23:47:05 -0400

    Add secure_password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params(params[:user].keys))
+      if @user.save
+        redirect_to @user
+      else
+        render 'new'
+      end
+  end
+
+  def show  
+  end
+
+  private
+    def user_params(user)
+      params.require(:user).permit(user)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
