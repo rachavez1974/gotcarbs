47ae1cc05895584e23e2d551c89633300ae5b906
Ramon Chavez <rachavez1974@gmail.com>
2019-09-05 16:12:52 -0400

    Add signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :users
+  get 'sessions/new'
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+
+  root to: 'static_pages#home'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   get '/about', to: 'static_pages#about', as: 'about'
   get '/menus', to: 'static_pages#menus', as: 'menus'
   get '/contact', to: 'static_pages#contact', as: 'contact'
