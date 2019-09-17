b998e5e175664d847dea532eed1cd497d737c74a
Ramon Chavez <rachavez1974@gmail.com>
2019-09-17 02:21:01 -0400

    Fix quantiy column

--- db/migrate/20190917060136_add_column_quantity_to_ordered_items.rb
+++ db/migrate/20190917060136_add_column_quantity_to_ordered_items.rb
@@ -1,5 +1,5 @@
 class AddColumnQuantityToOrderedItems < ActiveRecord::Migration[5.2]
   def change
-    add_column :ordered_items, :quanity, :integer
+    add_column :ordered_items, :quantity, :integer
   end
 end
