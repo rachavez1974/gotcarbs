014851b03dc084f2536bc72cb8ef7b9dceceff83
Ramon Chavez <rachavez1974@gmail.com>
2019-09-17 03:14:13 -0400

    Add create order

--- app/controllers/ordered_items_controller.rb
+++ app/controllers/ordered_items_controller.rb
@@ -1,5 +1,19 @@
 class OrderedItemsController < ApplicationController
+  
+
   def create
-    binding.pry
+    @order = current_order
+    @order.user_id = current_user.id
+    @order.ordered_items.new(item_params(params[:ordered_item].keys))
+    @order.save
+
+    session[:order_id] = @order.id
+    redirect_to menu_path
+  end
+
+  private
+
+  def item_params(item)
+    params.require(:ordered_item).permit(item)
   end
 end
