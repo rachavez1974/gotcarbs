acaebc2aef3b1dbe0252b67faa6c6f76119afdd3
Ramon Chavez <rachavez1974@gmail.com>
2019-09-20 02:39:21 -0400

    Can add items to cart via ordered_items

--- app/controllers/ordered_items_controller.rb
+++ app/controllers/ordered_items_controller.rb
@@ -1,14 +1,31 @@
 class OrderedItemsController < ApplicationController
   
 
-  def create
-    @order = current_order
-    @order.user_id = current_user.id
-    @order.ordered_items.new(item_params(params[:ordered_item].keys))
-    @order.save
+  def show
+    @cart  = current_user.cart
+  end
 
-    session[:order_id] = @order.id
-    redirect_to menu_path
+  def create
+    
+    if current_user.carts.first.nil?
+      @cart = current_user.carts.create
+      add_item_to_cart
+    else
+      @cart = current_user.carts.first 
+      @ordered_item = OrderedItem.find_by(:item_id => params[:ordered_item][:item_id])
+      if @cart.ordered_items.include?(@ordered_item)
+        @ordered_item.quantity += 1
+        @ordered_item.save
+        redirect_to menu_path
+      else
+        add_item_to_cart
+      end
+    end
+    
+  end
+
+  def edit
+    
   end
 
   private
@@ -16,4 +33,12 @@ class OrderedItemsController < ApplicationController
   def item_params(item)
     params.require(:ordered_item).permit(item)
   end
+
+  def add_item_to_cart
+    @ordered_item = OrderedItem.new(item_params(params[:ordered_item].keys))
+    @cart.ordered_items << @ordered_item
+    @cart.save
+    redirect_to menu_path
+  end
+
 end
