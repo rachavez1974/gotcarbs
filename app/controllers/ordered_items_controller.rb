cc1c00d49ab6ec94f84c0cd1a731032710d0218f
Ramon Chavez <rachavez1974@gmail.com>
2019-09-20 03:48:59 -0400

    Add addition and subtraction of items, and destroy

--- app/controllers/ordered_items_controller.rb
+++ app/controllers/ordered_items_controller.rb
@@ -1,12 +1,7 @@
 class OrderedItemsController < ApplicationController
   
 
-  def show
-    @cart  = current_user.cart
-  end
-
   def create
-    
     if current_user.carts.first.nil?
       @cart = current_user.carts.create
       add_item_to_cart
@@ -14,18 +9,25 @@ class OrderedItemsController < ApplicationController
       @cart = current_user.carts.first 
       @ordered_item = OrderedItem.find_by(:item_id => params[:ordered_item][:item_id])
       if @cart.ordered_items.include?(@ordered_item)
-        @ordered_item.quantity += 1
-        @ordered_item.save
-        redirect_to menu_path
+        increment_item_in_cart
       else
         add_item_to_cart
       end
-    end
-    
+    end 
   end
 
-  def edit
-    
+  def add_one
+    increment_item_in_cart(params)
+  end
+
+  def minus_one
+    decrement_item_in_cart(params)
+  end
+
+  def destroy
+    OrderedItem.find(params[:id]).destroy
+    flash[:success] = "Item deleted!"
+    redirect_to cart_path(current_user.carts.first)
   end
 
   private
@@ -39,6 +41,20 @@ class OrderedItemsController < ApplicationController
     @cart.ordered_items << @ordered_item
     @cart.save
     redirect_to menu_path
+  end
+
+  def increment_item_in_cart(params)
+    @ordered_item = OrderedItem.find_by(:item_id => params[:id])
+    @ordered_item.quantity += 1
+    @ordered_item.save
+    redirect_to cart_path(current_user.carts.first)
+  end
+
+  def decrement_item_in_cart(params)
+    @ordered_item = OrderedItem.find_by(:item_id => params[:id])
+    @ordered_item.quantity -= 1
+    @ordered_item.save
+    redirect_to cart_path(current_user.carts.first)
   end
 
 end
