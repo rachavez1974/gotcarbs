68b67ebd1ec2e07923d99566b769239d719724de
Ramon Chavez <rachavez1974@gmail.com>
2019-09-21 15:43:26 -0400

    Fix way items are save

--- app/controllers/ordered_items_controller.rb
+++ app/controllers/ordered_items_controller.rb
@@ -16,45 +16,45 @@ class OrderedItemsController < ApplicationController
   #   end 
   # end
 
-  # def add_one
-  #   increment_item_in_cart(params)
-  # end
+  def add_one
+    @ordered_item = OrderedItem.find(params[:id])
+    increment_item_in_order
+  end
 
-  # def minus_one
-  #   decrement_item_in_cart(params)
-  # end
+  def minus_one
+    @ordered_item = OrderedItem.find(params[:id])
+    decrement_item_in_order
+  end
 
-  # def destroy
-  #   OrderedItem.find(params[:id]).destroy
-  #   flash[:success] = "Item deleted!"
-  #   redirect_to cart_path(current_user.carts.first)
-  # end
+  def destroy
+    @ordered_item = OrderedItem.find(params[:id]).destroy
+    flash[:success] = "Item deleted!"
+    redirect_to order_path(@ordered_item.order)
+  end
 
-  # private
+  private
 
-  # def item_params(item)
-  #   params.require(:ordered_item).permit(item)
-  # end
+  def item_params(item)
+    params.require(:ordered_item).permit(item)
+  end
 
-  # def add_item_to_cart
-  #   @ordered_item = OrderedItem.new(item_params(params[:ordered_item].keys))
-  #   @cart.ordered_items << @ordered_item
-  #   @cart.save
-  #   redirect_to menu_path
-  # end
+  def add_item_to_order
+    @ordered_item = OrderedItem.new(item_params(params[:ordered_item].keys))
+    @cart.ordered_items << @ordered_item
+    @cart.save
+    redirect_to menu_path
+  end
 
-  # def increment_item_in_cart(params)
-  #   @ordered_item = OrderedItem.find_by(:item_id => params[:id])
-  #   @ordered_item.quantity += 1
-  #   @ordered_item.save
-  #   redirect_to cart_path(current_user.carts.first)
-  # end
+  def increment_item_in_order
+    @ordered_item.quantity += 1
+    @ordered_item.save
+    redirect_to order_path(@ordered_item.order)
+  end
 
-  # def decrement_item_in_cart(params)
-  #   @ordered_item = OrderedItem.find_by(:item_id => params[:id])
-  #   @ordered_item.quantity -= 1
-  #   @ordered_item.save
-  #   redirect_to cart_path(current_user.carts.first)
-  # end
+  def decrement_item_in_order
+    @ordered_item.quantity -= 1
+    @ordered_item.save
+    redirect_to order_path(@ordered_item.order)
+  end
 
 end
