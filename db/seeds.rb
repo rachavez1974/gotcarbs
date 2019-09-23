961465fdf140ae30d3511d41e4838309f0a7a753
Ramon Chavez <rachavez1974@gmail.com>
2019-09-23 14:19:59 -0400

    Add orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,77 +6,92 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # Create a main sample user.
-User.create!(first_name:  "Ramon",
-             last_name: "Chavez",
-             email: "razor@gmail.org",
-             password:              "123",
-             password_confirmation: "123",
-             terms: true,
-             email_list: true,
-             birthday: nil,
-             admin: true,
-             phone_number: "2403508067",
-             :addresses_attributes => [  street_address: "Weework St",
-                                         number: "CS200",
-                                         city: "Silicon Valley",
-                                         zipcode: "20018",
-                                         state: "California",
-                                         address_type: 0,
-                                         unit_type: 1 ])
+# User.create!(first_name:  "Ramon",
+#              last_name: "Chavez",
+#              email: "razor@gmail.org",
+#              password:              "123",
+#              password_confirmation: "123",
+#              terms: true,
+#              email_list: true,
+#              birthday: nil,
+#              admin: true,
+#              phone_number: "2403508067",
+#              :addresses_attributes => [  street_address: "Weework St",
+#                                          number: "CS200",
+#                                          city: "Silicon Valley",
+#                                          zipcode: "20018",
+#                                          state: "California",
+#                                          address_type: 0,
+#                                          unit_type: 1 ])
 
-# # Generate a bunch of additional users with one address
-10.times do |n|
-  name = Faker::Name.name.split(" ")
-  first_name = name[0]
-  last_name  = name[1]
-  email = "example-#{n+1}@gotcarbs.org"
-  password = "password"
-  password_confirmation = "password"
-  terms = true
-  email_list = Faker::Boolean.boolean
-  birthday = Faker::Date.between(from: 99.years.ago, to: Date.today)
-  phone_number = Faker::Number.number(digits: 10)
+# # # Generate a bunch of additional users with one address
+# 10.times do |n|
+#   name = Faker::Name.name.split(" ")
+#   first_name = name[0]
+#   last_name  = name[1]
+#   email = "example-#{n+1}@gotcarbs.org"
+#   password = "password"
+#   password_confirmation = "password"
+#   terms = true
+#   email_list = Faker::Boolean.boolean
+#   birthday = Faker::Date.between(from: 99.years.ago, to: Date.today)
+#   phone_number = Faker::Number.number(digits: 10)
 
-  st = Faker::Address.street_address
-  num = Faker::Address.building_number
-  city = Faker::Address.city_prefix
-  code = Faker::Address.zip.slice(0, 5)
-  state = Faker::Address.state
-  ad_type = rand(0..3)
-  u_type = rand(0..3)
+#   st = Faker::Address.street_address
+#   num = Faker::Address.building_number
+#   city = Faker::Address.city_prefix
+#   code = Faker::Address.zip.slice(0, 5)
+#   state = Faker::Address.state
+#   ad_type = rand(0..3)
+#   u_type = rand(0..3)
 
-  User.create!(first_name:  first_name,
-               last_name: last_name,
-               email: email,
-               password:              password,
-               password_confirmation: password_confirmation,
-               terms: terms,
-               email_list: email_list,
-               birthday: birthday,
-               phone_number: phone_number,
-               :addresses_attributes => [street_address: st,
-                                         number: num,
-                                         city: city,
-                                         zipcode: code,
-                                         state: state,
-                                         address_type: ad_type,
-                                         unit_type: u_type])
-end
+#   User.create!(first_name:  first_name,
+#                last_name: last_name,
+#                email: email,
+#                password:              password,
+#                password_confirmation: password_confirmation,
+#                terms: terms,
+#                email_list: email_list,
+#                birthday: birthday,
+#                phone_number: phone_number,
+#                :addresses_attributes => [street_address: st,
+#                                          number: num,
+#                                          city: city,
+#                                          zipcode: code,
+#                                          state: state,
+#                                          address_type: ad_type,
+#                                          unit_type: u_type])
+# end
 
-#create breakfast items
-165.times do |n|
-  menu = rand(0..5)
-  section = rand(0..4)
-  name = Faker::Food.dish
-  desc = Faker::Food.description
-  price = Faker::Number.decimal(l_digits: 2, r_digits: 2)
-  av = true
+# #create breakfast items
+# 165.times do |n|
+#   menu = rand(0..5)
+#   section = rand(0..4)
+#   name = Faker::Food.dish
+#   desc = Faker::Food.description
+#   price = Faker::Number.decimal(l_digits: 2, r_digits: 2)
+#   av = true
 
-  Item.create!(name: name,
-               description: desc,
-               price: price,
-               menu_type: menu,
-               section: section,
-               availability: av)
+#   Item.create!(name: name,
+#                description: desc,
+#                price: price,
+#                menu_type: menu,
+#                section: section,
+#                availability: av)
 
-end
+# end
+
+#create orders for users
+users = User.all
+  users.each do |user|
+    5.times do |n|
+      item_id =rand(1..100)
+      item_quantity = rand(1..3)
+      # cart = Cart.create(:cart_id => user.id)
+      # Cart_Item.create(item_id: item_d, quatity: item_quantity, cart_id: cart.id)
+      order = user.orders.new
+      order.save
+      order.ordered_items.new(:quantity => item_quantity, :item_id => item_id)
+      order.save
+    end
+  end
